04690164989975fd6c7dcabf3fadceedb6534525
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-26 19:30:48 -0500

    Actualizacion, operadores de comparacion

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Escribe tu código aqui
+let edad=30
+let adulto=(false)
+if (edad > 18) { adulto=(true)
+} 
+let vidas=0
+let perdiste=(false)
+if(vidas<=0){
+    perdiste=(true)
+}
+let bottom=0
+let colision=(false)
+if (bottom===0){
+    colision=(true)
+}
